0bbf0ff3066cbf08ae172cf8d296fe95e139f6f4
bala-amypo <bala@amypo.in>
2025-12-18 09:13:18 +0000

    User commit

--- src/main/java/com/example/demo/entity/Student.java
+++ src/main/java/com/example/demo/entity/Student.java
@@ -7,30 +7,8 @@ public class Student{
     @Id
     @GeneratedValue(strategy=GenerationType.IDENTITY)
     private Long id;
-    @NotNull
     private String name;
-    
+    @NotBlank
     private String dept;
    
-    public Long getId() {
-        return id;
-    }
-    public void setId(Long id) {
-        this.id = id;
-    }
-    public String getName() {
-        return name;
-    }
-    public void setName(String name) {
-        this.name = name;
-    }
-   
-    public Student(Long id, String name, String dept) {
-        this.id = id;
-        this.name = name;
-        this.dept = dept;
-        
-    }
-    public Student() {
-    }
 }
