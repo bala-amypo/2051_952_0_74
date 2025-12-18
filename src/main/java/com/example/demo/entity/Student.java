c44c78e7926204cfa0b8bfd6c6ee7663d0300485
bala-amypo <bala@amypo.in>
2025-12-18 09:18:19 +0000

    User commit

--- src/main/java/com/example/demo/entity/Student.java
+++ src/main/java/com/example/demo/entity/Student.java
@@ -6,9 +6,51 @@ import jakarta.validation.constraints.*;
 public class Student{
     @Id
     @GeneratedValue(strategy=GenerationType.IDENTITY)
-    private Long id;
+     private Long id;
+     @NotBlank(message="Not null")
     private String name;
-    @NotBlank
     private String dept;
+    private float cgpa;
+    @Past()
+    private LocalDate dob;
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getDept() {
+        return dept;
+    }
+    public void setDept(String dept) {
+        this.dept = dept;
+    }
+    public float getCgpa() {
+        return cgpa;
+    }
+    public void setCgpa(float cgpa) {
+        this.cgpa = cgpa;
+    }
+    public LocalDate getDob() {
+        return dob;
+    }
+    public void setDob(LocalDate dob) {
+        this.dob = dob;
+    }
+    public Student(Long id, String name, String dept, float cgpa, LocalDate dob) {
+        this.id = id;
+        this.name = name;
+        this.dept = dept;
+        this.cgpa = cgpa;
+        this.dob = dob;
+    }
+    public Student() {
+    }
    
 }
