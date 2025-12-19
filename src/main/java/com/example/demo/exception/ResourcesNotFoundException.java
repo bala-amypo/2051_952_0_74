f7e901b5be88566204a8a9dc63108d7724178420
bala-amypo <bala@amypo.in>
2025-12-19 04:48:05 +0000

    User commit

---
+++ src/main/java/com/example/demo/exception/ResourcesNotFoundException.java
@@ -0,0 +1,8 @@
+package com.example.demo.exception;
+
+public class ResourceNotFoundException extends RuntimeException{
+    public  PasswordNotMatchCri(String msg){
+       super(msg);
+        
+    }
+}
